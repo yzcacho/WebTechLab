09986663275be01ebba84d70991f9f47397871a0
yzcacho <2162936@slu.edu.ph>
2018-04-09 01:39:01 +0800

    added joomla

---
+++ Joomla/administrator/logs/error.php
@@ -0,0 +1,8 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-03-28 10:11:39 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-03-28T10:11:39+00:00	INFO ::1	joomlafailure	Empty password not allowed.
+2018-03-28T10:11:44+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
